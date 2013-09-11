c9ea3ebf3a396a988d1c1bdce38f6d065b9ab98e
morehd <ctrlplus1daniel@gmail.com>
2013-09-11 18:12:10 -0400

    Use spaces for tabs throughout.

--- DMIconView.m
+++ DMIconView.m
@@ -60,7 +60,7 @@
     }
     
     // Set up indicator
-	UIImage *indicatorImage = [self.dataSource indicatorForIconView:self];
+    UIImage *indicatorImage = [self.dataSource indicatorForIconView:self];
     const float indicatorHeight = indicatorImage.size.height;
     
     // Resize frame
@@ -68,7 +68,7 @@
     frame.size.height = ICON_SIDE_LENGTH + indicatorHeight + (1.8 * SPACE);
     frame.size.width = [self getScreenBoundsForCurrentOrientation].size.width;
     [self setFrame:frame];
-	
+    
     // Initialize indicator if necessary
     if (!indicator) {
         indicator = [[UIImageView alloc] initWithImage:indicatorImage];
@@ -118,7 +118,7 @@
         [button setClipsToBounds:NO];
         [button setAdjustsImageWhenHighlighted:NO];
         [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
-		[button setBackgroundImage:[dataSource iconView:self backgroundAtIndex:i] forState:UIControlStateNormal];
+        [button setBackgroundImage:[dataSource iconView:self backgroundAtIndex:i] forState:UIControlStateNormal];
         
         CGRect buttonFrame;
         buttonFrame.origin.x = (SPACE * (i + 1)) + (ICON_SIDE_LENGTH * i);
@@ -141,8 +141,8 @@
         [label setShadowOffset:CGSizeMake(1.0, 1.0)];
         [label setTextAlignment:NSTextAlignmentCenter];
         [label setText:title];
-		[label sizeToFit];
-		[label setCenter:CGPointMake(button.frame.size.width / 2, button.frame.size.height)];
+        [label sizeToFit];
+        [label setCenter:CGPointMake(button.frame.size.width / 2, button.frame.size.height)];
         [button addSubview:label];
         
         // Add the button to the view
@@ -151,53 +151,53 @@
 }
 
 - (void)showItemAtIndex:(NSTimer *)timer {
-	CGFloat rectOriginX = (ICON_SIDE_LENGTH + SPACE) * ([[timer userInfo] intValue] + 1);
-	[self scrollRectToVisible:CGRectMake(rectOriginX, 0, 1, 1) animated:NO];
+    CGFloat rectOriginX = (ICON_SIDE_LENGTH + SPACE) * ([[timer userInfo] intValue] + 1);
+    [self scrollRectToVisible:CGRectMake(rectOriginX, 0, 1, 1) animated:NO];
 }
 
 - (void)showAnimated:(BOOL)animated {
-	if (![self isHidden])
-		return;
+    if (![self isHidden])
+        return;
     
-	if (animated) {
-		[UIView animateWithDuration:.3 animations:^{
-			[self setFrame:onScreenFrame];
-		}];
-	} else {
-		[self setFrame:onScreenFrame];
-	}
+    if (animated) {
+        [UIView animateWithDuration:.3 animations:^{
+            [self setFrame:onScreenFrame];
+        }];
+    } else {
+        [self setFrame:onScreenFrame];
+    }
 }
 
 - (void)hideAnimated:(BOOL)animated {
-	if ([self isHidden])
-		return;
+    if ([self isHidden])
+        return;
     
-	if (animated) {
-		[UIView animateWithDuration:.3 animations:^{
-			[self setFrame:offScreenFrame];
-		}];
-	} else {
-		[self setFrame:offScreenFrame];
-	}
+    if (animated) {
+        [UIView animateWithDuration:.3 animations:^{
+            [self setFrame:offScreenFrame];
+        }];
+    } else {
+        [self setFrame:offScreenFrame];
+    }
 }
 
 - (void)toggleHiddenAnimated:(BOOL)animated {
-	if ([self isHidden])
-		[self showAnimated:animated];
-	else
-		[self hideAnimated:animated];
+    if ([self isHidden])
+        [self showAnimated:animated];
+    else
+        [self hideAnimated:animated];
 }
 
 - (BOOL)isHidden {
-	return CGRectEqualToRect(self.frame, offScreenFrame);
+    return CGRectEqualToRect(self.frame, offScreenFrame);
 }
 
 - (void)moveIndicatorToIndex:(int)index {
-	if (self.delegate && [self.delegate iconView:self didSelectItemAtIndex:index]) {
-		CGPoint center = indicator.center;
-		center.x = (SPACE * (index + 1)) + (ICON_SIDE_LENGTH * (index + 0.5));
-		indicator.center = center;
-	}
+    if (self.delegate && [self.delegate iconView:self didSelectItemAtIndex:index]) {
+        CGPoint center = indicator.center;
+        center.x = (SPACE * (index + 1)) + (ICON_SIDE_LENGTH * (index + 0.5));
+        indicator.center = center;
+    }
 }
 
 - (void)buttonPressed:(id)sender
@@ -205,11 +205,11 @@
     NSInteger buttonIndex = [sender tag];
     
     // Alert the delegate
-	BOOL shouldMoveIndicator = YES;
+    BOOL shouldMoveIndicator = YES;
     if (self.delegate) {
         shouldMoveIndicator = [self.delegate iconView:self didSelectItemAtIndex:buttonIndex];
     }
-	
+    
     // Update content offset and indicator
     [UIView animateWithDuration:.2 animations:^{
         // If the button selected is partially off the screen, adjust the content offset to bring it on the screen.
@@ -223,11 +223,11 @@
             [self setContentOffset:CGPointMake((buttonFrame.origin.x + buttonFrame.size.width + SPACE - self.frame.size.width), 0)];
         }
         
-		if (shouldMoveIndicator) {
-			CGPoint center = indicator.center;
-			center.x = (SPACE * (buttonIndex + 1)) + (ICON_SIDE_LENGTH * (buttonIndex + 0.5));
-			indicator.center = center;
-		}
+        if (shouldMoveIndicator) {
+            CGPoint center = indicator.center;
+            center.x = (SPACE * (buttonIndex + 1)) + (ICON_SIDE_LENGTH * (buttonIndex + 0.5));
+            indicator.center = center;
+        }
     }];
 }
 
